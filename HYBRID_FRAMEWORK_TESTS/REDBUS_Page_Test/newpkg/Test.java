94b2d84cffdf43bee5444cad00d44e6fa5d70677
romasamreen <samreenmulla17395@gmail.com>
2020-02-17 19:02:12 +0530

    adding test

---
+++ HYBRID_FRAMEWORK_TESTS/REDBUS_Page_Test/newpkg/Test.java
@@ -0,0 +1,15 @@
+package newpkg;
+import java.io.*;
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello demo");
+
+	}
+
+}
